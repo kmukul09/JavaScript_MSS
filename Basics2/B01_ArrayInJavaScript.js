b4d41c58ecb5ace752078d3c4245faf24231efc7
Mukul Kumar <92209350+kmukul09@users.noreply.github.com>
2024-04-06 17:35:03 +0000

    Array in Java Script

---
+++ Basics2/B01_ArrayInJavaScript.js
@@ -0,0 +1,8 @@
+
+// For Documentation read MDN document of Array
+
+const myArray = [0,1,2,3,4,5]; // Created an array
+const myHeros = ["Shaktiman", "IronMan", "BatMan", 45, 89, 34] // an Array can have data of different type
+console.log(myArray[3]);
+
+   
