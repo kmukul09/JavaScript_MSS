69e8b1fef11293314c62298363e65f357203d1dd
Mukul Kumar <92209350+kmukul09@users.noreply.github.com>
2024-04-10 17:03:13 +0000

    This, Function and Arrow Fun, with different way of writting function

--- Basics3/B04_ThisAndArrowFunctionInJS.js
+++ Basics3/B04_ThisAndArrowFunctionInJS.js
@@ -2,10 +2,10 @@
 
 
 let user = {
-    username : "Mukul",
+    username: "Mukul",
     price: 999,
 
-    welcomeMessage: function(){
+    welcomeMessage: function () {
         console.log(`${this.username}, Welcome to Website`); // this is use here
 
     }
@@ -26,7 +26,7 @@ console.log(this); // It returns the empty object
 
 // let's see another example 
 
-function chai(){
+function chai() {
     let username = "Mukul"
     //console.log(this); // we can see a lot ot info in terminal 
     console.log(this.username); // output: Undefined
@@ -41,7 +41,7 @@ chai();
 // Now Let's understand normal function and arrow function 
 
 // Normal function : also expression
-const el1 =  function chaitime() {
+const el1 = function chaitime() {
     let username = "Ankit";
     console.log(username);
 }
@@ -53,6 +53,72 @@ const el2 = () => {
     let username = "UserDEmon";
     console.log(username);
 }
+
+console.log(el2);
+
+// What is difference between function and arrow function 
+/*
+-> Main difference between function and Arrow if how they treat "this" keyword 
+   
+    -> In function "this" is dynamically scoped hence its value is determine by how the function is called 
+    -> In Arrow function "this" is lexically scoped means it inherits the value of "this" from its surrounding code
+    
+
+
+    1.Regular functions have dynamic this binding and their own arguments object, 
+     while arrow functions have lexically scoped this and lack their own arguments object.
+
+
+    2.Regular functions can be used as constructors with new, support super keyword, 
+     and have a prototype property; arrow functions cannot.
+
+
+    3.Arrow functions have a more concise syntax compared to regular functions.
+
+    4.Arrow functions are especially useful for concise anonymous functions and preserving this context in callback functions.
+*/
+
+
+// Now arrow function: 
+
+/*
+const addtwo = (num1, num2) => {
+    return num1+num2;
+}
+
+console.log(addtwo(23, 34));
+*/
+
+// now since above is a one line function code so we can write it without curley braces in a single line 
+
+// type 1
+const addtwo = (num1, num2) => num1+num2; // with return and curley braces
+console.log(addtwo(23, 34));
+
+// type 2
+const addthree = (num1, num2, num3) => (num1+num2+num3); // if you don't want confusion then use paranthesis 
+
+// Above two techniques are gonna be used in react 
+
+
+// type 3
+// but if your function is not a one liner then it will be a problem so go with normal flow 
+
+console.log(addthree(12, 13, 14));
+
+
+// If you have to return object then 
+const obj = () => ({username:"React"});
+// console.log("If you want to return object, then enclose it in curley braces : " + obj());
+
+// Arrow function is mostly used in loops 
+// ex:
+// const arr1 = [1,2,3,4,5,6,7,8];
+// arr1.forEach(()=>{}) // Like this 
+
+
+
+
 
 
 
