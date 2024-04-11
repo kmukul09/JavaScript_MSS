9249ccc1707bdbff35fb1051d6654243dc613c22
Mukul Kumar <92209350+kmukul09@users.noreply.github.com>
2024-04-11 11:49:43 +0000

    If ElseIf esle ans its use cases

---
+++ Dir04_ControlFlow/D01_IfElseIfElse.js
@@ -0,0 +1,17 @@
+// if
+
+if( true){
+    // statement 
+}else{
+    // Alternet  
+}
+
+// <, >, <=, >=, ==, ===
+
+// one line if else can also be written
+const balance = 1000;
+if(balance>500) console.log("test"), console.log("DontUseMultipleLinesThisWay");
+// ek line wale if else me jada lines mat likho and semi colon lgana mat bhule good practice 
+
+// we can use if else if and else to check multiple conditions 
+// nested loops also are there
