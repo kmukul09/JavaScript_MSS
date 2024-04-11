6924b765c4b7f10aeb1ef4917b7f111a6a66479d
Mukul Kumar <92209350+kmukul09@users.noreply.github.com>
2024-04-11 12:03:35 +0000

    Switch Case and Its Uses

---
+++ Dir04_ControlFlow/D02_Switch.js
@@ -0,0 +1,57 @@
+// Switch
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    
+    default:
+        console.log("Tera Antim Mhina Es Dharti Par");
+        break;
+}
+
+// Switch kis type ka rkhna hai apne hisab se soch lia krna 
+// and case v usi type ka rkhna fir 
+
+
+const month_ = "Mar"
+
+switch (month_) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "Mar":
+        console.log("March");
+        break;
+    case "Apr":
+        console.log("April");
+        break;
+    
+    default:
+        console.log("Tera Antim Mhina Es Dharti Par");
+        break;
+}
+
